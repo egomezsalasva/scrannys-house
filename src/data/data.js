2562ba76cbb678ec63ea1adef7aeffe1eedaa5a6
Kike <kike@Kikes-MacBook-Pro.local>
2020-02-09 23:24:11 +0100

    Delete Public Img Folder add Netlify Img route to DB

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ const PRODUCTS_DB = [
         title: "Walkers Roast Chicken",
         weight: "25g",
         price: 0.80.toFixed(2),
-        img: "",
+        img: "https://www.scrannyshouse.com/static/media/WalkersRoastChicken.8c04d2c9.png",
         category: "crisps",
         cartQuantity: 0,
     },
